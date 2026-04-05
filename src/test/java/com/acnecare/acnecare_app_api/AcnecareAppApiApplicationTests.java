e0d4cd23304ab9b5c7fbc9a05fa6b9f7555177a0
tranbatong <tongxayda00@gmail.com>
2026-04-05 13:22:46 +0700

    first commit

---
+++ src/test/java/com/acnecare/acnecare_app_api/AcnecareAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acnecare.acnecare_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcnecareAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
