85b4586c47013eb855629d86678b9a0fded2b2af
Nguyễn Văn Tú Anh <145957834+nguyenAnh132@users.noreply.github.com>
2026-04-05 14:39:18 +0700

    feat: add user, role and invalidated token entity

---
+++ src/main/java/com/acnecare/acnecare_app_api/identity/entity/Role.java
@@ -0,0 +1,26 @@
+package com.acnecare.acnecare_app_api.identity.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+    @Id
+    @Column(nullable = false, length = 64)
+    private String name;
+
+    private String description;
+}
