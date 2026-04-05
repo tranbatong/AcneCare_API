86862a19b22be92c7ecd43d2b688f68b8486bec6
Nguyễn Văn Tú Anh <145957834+nguyenAnh132@users.noreply.github.com>
2026-04-05 14:40:49 +0700

    feat: add mapper and dto for identity domain

---
+++ src/main/java/com/acnecare/acnecare_app_api/identity/dto/request/LogoutRequest.java
@@ -0,0 +1,21 @@
+package com.acnecare.acnecare_app_api.identity.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import jakarta.annotation.Nullable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LogoutRequest {
+    @Nullable
+    String accessToken;
+    @Nullable
+    String refreshToken;
+}
